1fe48d45c54f144d5efba5b056fc107f808855d5
Claire Munyole <munyolec@gmail.com>
2020-02-17 12:17:22 -0800

    Updates

---
+++ precache-manifest.5762c56915a4ac81f1d16010b0adc487.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "da060feabca331c10805df96a5647b44",
+    "url": "/contactmanager/index.html"
+  },
+  {
+    "revision": "8f53865008128bbfc8b0",
+    "url": "/contactmanager/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "6f636585f27e71155ba9",
+    "url": "/contactmanager/static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "8f53865008128bbfc8b0",
+    "url": "/contactmanager/static/js/2.d1b4883c.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/contactmanager/static/js/2.d1b4883c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "6f636585f27e71155ba9",
+    "url": "/contactmanager/static/js/main.6135b0ce.chunk.js"
+  },
+  {
+    "revision": "d86057960233a7428d99",
+    "url": "/contactmanager/static/js/runtime-main.2ce66dc9.js"
+  }
+]);
